cd9a05d64e21c553a61bfeaab02fdcac9e278997
DESKTOP-1S4T47J\dlwor <xdl_line@hotmail.com>
2018-06-09 16:55:01 +0800

    Link login Button to Browse Page.

--- app/src/main/java/com/example/gaominyu/slease/LoginActivity.java
+++ app/src/main/java/com/example/gaominyu/slease/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.gaominyu.slease;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button = (Button)findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, BrowseActivity.class));
+            }
+        });
     }
 }
