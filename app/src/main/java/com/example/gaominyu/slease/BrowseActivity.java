af69600f726e95af0267a901d2133d79dbd38db5
DESKTOP-1S4T47J\dlwor <xdl_line@hotmail.com>
2018-06-10 17:22:40 +0800

    Changed icon and Added res/menu/directory for toolbar.

--- app/src/main/java/com/example/gaominyu/slease/BrowseActivity.java
+++ app/src/main/java/com/example/gaominyu/slease/BrowseActivity.java
@@ -2,6 +2,7 @@ package com.example.gaominyu.slease;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.support.v7.widget.Toolbar;
 
 public class BrowseActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class BrowseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+
+        Toolbar topToolbar = (Toolbar) findViewById(R.id.top_toolbar);
+        setSupportActionBar(topToolbar);
     }
 }
