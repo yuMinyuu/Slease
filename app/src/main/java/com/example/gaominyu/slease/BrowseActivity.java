f23eae967f5bce6eaace0ee5448b4636000bde46
DESKTOP-1S4T47J\dlwor <xdl_line@hotmail.com>
2018-06-11 01:10:57 +0800

    Appear menu actions on top action bar.

--- app/src/main/java/com/example/gaominyu/slease/BrowseActivity.java
+++ app/src/main/java/com/example/gaominyu/slease/BrowseActivity.java
@@ -2,7 +2,10 @@ package com.example.gaominyu.slease;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.support.v7.widget.SearchView;
         import android.support.v7.widget.Toolbar;
+        import android.view.Menu;
+        import android.view.MenuItem;
 
 public class BrowseActivity extends AppCompatActivity {
 
@@ -11,7 +14,40 @@ public class BrowseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
 
-        Toolbar topToolbar = (Toolbar) findViewById(R.id.top_toolbar);
+        Toolbar topToolbar = findViewById(R.id.top_toolbar);
         setSupportActionBar(topToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Set up the menu options on top tool bar.
+        getMenuInflater().inflate(R.menu.directory, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_searchs);
+        SearchView searchView =
+                (SearchView) searchItem.getActionView();
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_searchs:
+                // User chose the "Search" action, activate text input and search button
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
