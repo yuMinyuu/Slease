f75405c9e0a1cc010b8e34f0f67642f2ce7d856c
DESKTOP-1S4T47J\dlwor <xdl_line@hotmail.com>
2018-06-14 10:42:40 +0800

    Link from fab to CreateActivity. Add Spinner and other layouts

--- app/src/main/java/com/example/gaominyu/slease/BrowseActivity.java
+++ app/src/main/java/com/example/gaominyu/slease/BrowseActivity.java
@@ -1,11 +1,15 @@
 package com.example.gaominyu.slease;
 
+        import android.content.Intent;
+        import android.support.design.widget.FloatingActionButton;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.support.v7.widget.SearchView;
         import android.support.v7.widget.Toolbar;
         import android.view.Menu;
         import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
 
 public class BrowseActivity extends AppCompatActivity {
 
@@ -16,6 +20,14 @@ public class BrowseActivity extends AppCompatActivity {
 
         Toolbar topToolbar = findViewById(R.id.top_toolbar);
         setSupportActionBar(topToolbar);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BrowseActivity.this, CreateActivity.class));
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/gaominyu/slease/CreateActivity.java
+++ app/src/main/java/com/example/gaominyu/slease/CreateActivity.java
@@ -2,6 +2,14 @@ package com.example.gaominyu.slease;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateActivity extends AppCompatActivity {
 
@@ -9,5 +17,44 @@ public class CreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        // Programmatically initialize the spinner for categories
+        initSimpleSpinner();
+    }
+
+    private void initSimpleSpinner() {
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Spinner Drop down elements
+        List<String> languages = new ArrayList<String>();
+        languages.add("Andorid");
+        languages.add("IOS");
+        languages.add("PHP");
+        languages.add("Java");
+        languages.add(".Net");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, languages);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String item = parent.getItemAtPosition(position).toString();
+
+                Toast.makeText(parent.getContext(), "Android Simple Spinner Example Output..." + item, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
