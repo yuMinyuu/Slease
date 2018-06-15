cdeb6f0957677548b989cd5573efc26ab8621850
DESKTOP-1S4T47J\dlwor <xdl_line@hotmail.com>
2018-06-15 17:07:23 +0800

    Add camera frameview into the app itself.

--- app/src/main/java/com/example/gaominyu/slease/PhotoActivity.java
+++ app/src/main/java/com/example/gaominyu/slease/PhotoActivity.java
@@ -2,39 +2,52 @@ package com.example.gaominyu.slease;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.hardware.Camera;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 public class PhotoActivity extends AppCompatActivity {
 
-    ImageView photoPreview;
+    FrameLayout photoFrame;
+    Camera camera;
+    ShowCamera showCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
 
+        // Object instantiation
         Button btnTakePhoto = findViewById(R.id.photo_takePhoto);
-        photoPreview = findViewById(R.id.photo_preview);
+        photoFrame = findViewById(R.id.photo_frame);
 
-        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
-            }
-        });
+        // Open the camera
+        camera = Camera.open();
+
+        // Show a surface view of the camera inside our FrameLayout
+        showCamera = new ShowCamera(this, camera);
+        photoFrame.addView(showCamera);
+
+
+
+//        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(intent, 0);
+//            }
+//        });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (
-                Bitmap)data.getExtras().get("data");
-        photoPreview.setImageBitmap(bitmap);
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+//        photoPreview.setImageBitmap(bitmap);
+//    }
 }
