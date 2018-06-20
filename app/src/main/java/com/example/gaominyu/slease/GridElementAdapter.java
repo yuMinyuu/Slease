8cf794007e24d65245a7507ec8146b47ee7824b4
DESKTOP-1S4T47J\dlwor <xdl_line@hotmail.com>
2018-06-20 10:46:03 +0800

    Add image taken to the linear layout of imageviews.

---
+++ app/src/main/java/com/example/gaominyu/slease/GridElementAdapter.java
@@ -0,0 +1,70 @@
+package com.example.gaominyu.slease;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GridElementAdapter extends RecyclerView.Adapter<GridElementAdapter.SimpleViewHolder>{
+    private Context context;
+    private List<String> elements;
+
+    public GridElementAdapter(Context context){
+        this.context = context;
+        this.elements = new ArrayList<String>();
+        // Fill dummy list
+        for(int i = 0; i < 40 ; i++){
+            this.elements.add(i, "Position : " + i);
+        }
+    }
+
+    public static class SimpleViewHolder extends RecyclerView.ViewHolder {
+        public final ImageView img1, img2, img3, img4, img5;
+        public final Button button;
+
+        public SimpleViewHolder(View view) {
+            super(view);
+            button = view.findViewById(R.id.button);
+            img1 = (ImageView) view.findViewById(R.id.imageView1);
+            img2 = (ImageView) view.findViewById(R.id.imageView2);
+            img3 = (ImageView) view.findViewById(R.id.imageView3);
+            img4 = (ImageView) view.findViewById(R.id.imageView4);
+            img5 = (ImageView) view.findViewById(R.id.imageView5);
+        }
+    }
+
+    @Override
+    public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(this.context).inflate(R.layout.partial_photo, parent, false);
+        return new SimpleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(SimpleViewHolder holder, final int position) {
+        holder.button.setText(elements.get(position));
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Position =" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.elements.size();
+    }
+}
--- app/src/main/java/com/example/gaominyu/slease/PhotoActivity.java
+++ app/src/main/java/com/example/gaominyu/slease/PhotoActivity.java
@@ -1,14 +1,19 @@
 package com.example.gaominyu.slease;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.v17.leanback.widget.HorizontalGridView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.File;
@@ -25,9 +30,11 @@ import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 public class PhotoActivity extends AppCompatActivity {
 
     private static final String TAG = "PhotoActivity";
-    FrameLayout photoFrame;
-    Camera camera;
-    CameraView cameraView;
+    private FrameLayout photoFrame;
+    private Camera camera;
+    private CameraView cameraView;
+    private HorizontalGridView horizontalGridView;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,7 @@ public class PhotoActivity extends AppCompatActivity {
         // Object instantiation
         Button btnTakePhoto = findViewById(R.id.photo_button1);
         photoFrame = findViewById(R.id.photo_frame);
+        linearLayout = findViewById(R.id.photo_thumbnails);
 
         // Open the camera
         camera = Camera.open();
@@ -44,6 +52,11 @@ public class PhotoActivity extends AppCompatActivity {
         // Show a surface view of the camera inside our FrameLayout
         cameraView = new CameraView(this, camera);
         photoFrame.addView(cameraView);
+
+//        // Initialize the HorizontalScrollView for images taken
+//        horizontalGridView = findViewById(R.id.photo_gridView);
+//        GridElementAdapter adapter = new GridElementAdapter(this);
+//        horizontalGridView.setAdapter(adapter);
 
         // Check if max 9 pics is met before allowing another picture to be taken
         btnTakePhoto.setOnClickListener(new View.OnClickListener() {
@@ -65,8 +78,15 @@ public class PhotoActivity extends AppCompatActivity {
                     // Save this picture to the page's list of thumbnails
                     Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_LONG).show();
 
+                    ImageView image = (ImageView) linearLayout.getChildAt(0);
+                    Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    image.setImageBitmap(Bitmap.createScaledBitmap(bmp, 320,426, false));
+                    image.setVisibility(View.VISIBLE);
+
                     // Restart camera after taking one picture
                     camera.startPreview();
+
+
                 }
             });
         }
