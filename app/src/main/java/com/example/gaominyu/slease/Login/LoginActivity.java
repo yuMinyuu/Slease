590a7350b420fe433252d1ce5d0850dff1cad8dc
DESKTOP-1S4T47J\dlwor <xdl_line@hotmail.com>
2018-07-02 02:29:42 +0800

    Able to login using Facebook.

--- app/src/main/java/com/example/gaominyu/slease/Login/LoginActivity.java
+++ app/src/main/java/com/example/gaominyu/slease/Login/LoginActivity.java
@@ -1,13 +1,9 @@
 package com.example.gaominyu.slease.Login;
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,8 +16,11 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -30,22 +29,28 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private LoginButton mFacebookLoginBtn;
+    private Button mLoginBtn;
+
     private CallbackManager mCallbackManager;
-
     private FirebaseAuth mAuth;
-
-    private Button mFacebookLoginBtn, mLoginBtn;
+    private Bundle userData;
 
     private static final String TAG;
 
     static {
-        TAG = "FACELOG";
+        TAG = "FACEBOOK_LOG";
     }
 
     @Override
@@ -54,16 +59,15 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-
+        // Declare layout element and Auth instances
         mFacebookLoginBtn = findViewById(R.id.btn_facebookLogin) ;
-
         mLoginBtn = findViewById(R.id.loginBtn);
+        mAuth = FirebaseAuth.getInstance();
 
         // Initialize Facebook Login button
         mCallbackManager = CallbackManager.Factory.create();
 
+        // Initialize Normal Login (fake)
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,9 +85,17 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
                         Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                        handleFacebookAccessToken(loginResult.getAccessToken());
-                        //Intent intent = new Intent(LoginActivity.this, BrowseActivity.class);
-                        //startActivity(intent);
+                        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                        userData = intent.getExtras();
+                        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.d("response", response.toString());
+                                setData(object, userData);
+                            }
+                        });
+                        request.executeAsync();
+                        startActivity(intent, userData);
                     }
 
                     @Override
@@ -101,23 +113,12 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-    }
 
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            updateUI();
+        // If already logged in, go to BrowseActivity
+        if(AccessToken.getCurrentAccessToken() != null) {
+            Toast.makeText(this, "Already logged in" + AccessToken.getCurrentAccessToken().getUserId(), Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(LoginActivity.this, BrowseActivity.class));
         }
-
-
-    }
-
-    private void updateUI() {
-        //Toast.makeText(LoginActivity.this, "You are logged in.", Toast.LENGTH_LONG).show();
-        Intent account =new Intent(LoginActivity.this, BrowseActivity.class);
-        finish();
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -127,36 +128,23 @@ public class LoginActivity extends AppCompatActivity {
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void handleFacebookAccessToken(AccessToken token) {
-        Log.d(TAG, "handleFacebookAccessToken:" + token);
+    private  void setData (JSONObject object, Bundle userData) {
+        try{
+            URL profile__picture = new URL("https://graph.facebook.com/" + object.getString("id") + "/picture?width=250&height=250");
 
-        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
+            // Load user's Facebook Data into the app here
+            Log.d("data", object.getString("id"));
+            //userData.putString("userId", object.getString("id"));
+            Log.d("data", object.getString("email"));
+            userData.putString("email", object.getString("email"));
+            Log.d("data", object.getString("name"));
+            userData.putString("name", object.getString("name"));
 
-                            mFacebookLoginBtn.setEnabled(true);
-
-                            updateUI();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
-                            mFacebookLoginBtn.setEnabled(true);
-
-                            updateUI();
-                        }
-
-                        // ...
-                    }
-                });
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
 }
