4bff83c3665b110438a15ec7d2ab5fdb8c2ed8f7
DESKTOP-1S4T47J\dlwor <xdl_line@hotmail.com>
2018-07-01 23:03:42 +0800

    Enable signupactivity to upload user info to firebase

--- app/src/main/java/com/example/gaominyu/slease/Login/SignupActivity.java
+++ app/src/main/java/com/example/gaominyu/slease/Login/SignupActivity.java
@@ -2,14 +2,140 @@ package com.example.gaominyu.slease.Login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.gaominyu.slease.Model.User;
 import com.example.gaominyu.slease.R;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class SignupActivity extends AppCompatActivity {
+
+    private static final String TAG = SignupActivity.class.getSimpleName();
+    private TextView details;
+    private EditText inputDisplayName, inputDescription;
+    private Button btnSave;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+
+    private String userId;
+
+    public SignupActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // Declaration of layout elements
+        details = findViewById(R.id.text_UserName);
+        inputDisplayName = findViewById(R.id.editText_DisplayName);
+        inputDescription = findViewById(R.id.editText_Description);
+        btnSave = findViewById(R.id.button_save);
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+
+        // get reference to 'users' node
+        mFirebaseDatabase = mFirebaseInstance.getReference("users");
+
+        // Save / update the user
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String displayName = inputDisplayName.getText().toString();
+                String description = inputDescription.getText().toString();
+
+                // Check for already existed userId
+                if (TextUtils.isEmpty(userId)) {
+                    createUser(displayName, description);
+                } else {
+                    updateUser(displayName, description);
+                }
+            }
+        });
+
+        toggleButton();
+    }
+
+    // Changing button text
+    private void toggleButton() {
+        if (TextUtils.isEmpty(userId)) {
+            btnSave.setText("Save");
+        } else {
+            btnSave.setText("Update");
+        }
+    }
+
+    /**
+     * Creating new user node under 'users'
+     */
+    private void createUser(String displayName, String description) {
+        // TODO
+        // In real apps this userId should be fetched
+        // by implementing firebase auth
+        if (TextUtils.isEmpty(userId)) {
+            userId = mFirebaseDatabase.push().getKey();
+        }
+
+        User user = new User(displayName, description);
+
+        mFirebaseDatabase.child(userId).setValue(user);
+
+        addUserChangeListener();
+    }
+
+    /**
+     * User data change listener
+     */
+    private void addUserChangeListener() {
+        // User data change listener
+        mFirebaseDatabase.child(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                // Check for null
+                if (user == null) {
+                    Log.e(TAG, "User data is null!");
+                    return;
+                }
+
+                Log.e(TAG, "User data is changed!" + user.displayName + ", " + user.description);
+
+                // Display newly updated name and email
+                details.setText(user.displayName + ", " + user.description);
+
+                // clear edit text
+                inputDisplayName.setText("");
+                inputDescription.setText("");
+
+                toggleButton();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.e(TAG, "Failed to read user", error.toException());
+            }
+        });
+    }
+
+    private void updateUser(String displayName, String description) {
+        // updating the user via child nodes
+        if (!TextUtils.isEmpty(displayName))
+            mFirebaseDatabase.child(userId).child("displayName").setValue(displayName);
+
+        if (!TextUtils.isEmpty(description))
+            mFirebaseDatabase.child(userId).child("description").setValue(description);
     }
 }
--- app/src/main/java/com/example/gaominyu/slease/Model/User.java
+++ app/src/main/java/com/example/gaominyu/slease/Model/User.java
@@ -5,16 +5,16 @@ import com.google.firebase.database.IgnoreExtraProperties;
 @IgnoreExtraProperties
 public class User {
 
-    public String name;
-    public String email;
+    public String displayName;
+    public String description;
 
     // Default constructor required for calls to
     // DataSnapshot.getValue(User.class)
     public User() {
     }
 
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
+    public User(String displayName, String description) {
+        this.displayName = displayName;
+        this.description = description;
     }
 }
