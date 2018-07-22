2ffdc3a4be4ca72b151e41ffa8fc4cbccc86921c
DESKTOP-1S4T47J\dlwor <xdl_line@hotmail.com>
2018-07-22 10:09:43 +0800

    add itemactivity and related layout

---
+++ app/src/main/java/com/example/gaominyu/slease/Detail/ItemActivity.java
@@ -0,0 +1,15 @@
+package com.example.gaominyu.slease.Detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gaominyu.slease.R;
+
+public class ItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+    }
+}
